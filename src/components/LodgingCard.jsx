de3ac22012791df800ba9d2e129637b52fbd912f
Eri Schon <erischon@gmail.com>
2022-09-14 12:11:24 +0200

    home page design in progress

---
+++ src/components/LodgingCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LodgingCard = () => {
+  return <div>LodgingCard</div>;
+};
+
+export default LodgingCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import background from "../assets/hero-bg-desktop.png";
+
 const HomePage = () => {
   const [lodgingData, setLodgingData] = useState([]);
 
   const getData = async () => {
-    const response = await axios.get("logements.json");
+    const response = await axios.get("data/logements.json");
     const lodging = response.data;
     setLodgingData(lodging);
   };
@@ -15,10 +17,26 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
-      HomePage
-      {lodgingData[0]?.title}
-    </div>
+    <main>
+      <section
+        className="hero__section"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <h1>
+          Chez vous, <span>partout et ailleurs</span>
+        </h1>
+      </section>
+
+      <section className="lodging__section">
+        {lodgingData &&
+          lodgingData.map((lodging) => (
+            <article className="lodging__card">
+              <img src={lodging?.pictures[0]} alt={lodging?.title} />
+              <h2>{lodging?.title}</h2>
+            </article>
+          ))}
+      </section>
+    </main>
   );
 };
 
