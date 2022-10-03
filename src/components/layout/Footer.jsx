1374bb76651608eb85d6823e35391cbb6c0ab4ce
Eri Schon <erischon@gmail.com>
2022-10-03 09:42:09 +0200

    footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,12 @@
+import kasaLogo from "../../assets/kasa-logo-footer.svg";
+
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <img src={kasaLogo} alt="Kasa" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
 };
 
 export default footer;
